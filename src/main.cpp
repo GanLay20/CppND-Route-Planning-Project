5683ad0f2f68ed615849442f730ea2a8394cd07c
GanLay <46540937+GGaannLLaayy@users.noreply.github.com>
2021-02-06 01:41:38 +0630

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 using namespace std::experimental;
 
+
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
     std::ifstream is{path, std::ios::binary | std::ios::ate};
@@ -55,12 +56,26 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+  float start_x,start_y,end_x,end_y;
+  std::cout << "Enter start point for x,y and end point for x,y \n" ;
+  
+  std::cout << "Enter start x value : ";
+  std::cin >> start_x;
+  
+  std::cout << "Enter start y value : ";
+  std::cin >> start_y;
+  
+  std::cout << "Enter end x value : ";
+  std::cin >> end_x;
+  
+  std::cout << "Enter end y value : ";
+  std::cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
